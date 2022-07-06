df8a91220419deef079b3c212f25273ee8a2d6fb
Jelle VdP <jellevandepoel@gmail.com>
2022-07-06 12:29:52 +0200

    completed some more exercises

--- index.php
+++ index.php
@@ -120,5 +120,172 @@ $totalCustomersNevadaNewYorkNotUS = mysqli_fetch_all($result, MYSQLI_ASSOC);
 echo "<p>The amount of customers living in Nevada, New York or abroad is " . $totalCustomersNevadaNewYorkNotUS[0]["total"] . ".</p>";
 echo "<br>";
 
+
+// Question 11
+echo "<h3>Question 11: How many customers do we have with the following conditions (only 1 query needed):  - Living in the state Nevada or New York OR - Living outside the USA and with a credit limit above 1000 dollar?</h3>";
+
+$sql = "SELECT COUNT(*) as total FROM customers WHERE (state='Nevada' OR 'New York' OR country<>'United States') AND creditLimit > 1000";
+$result = mysqli_query($conn, $sql);
+$totalCustomersNevadaNewYorkNotUSWithHighCreditLimit = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>The amount of customers living in Nevada, New York or abroad, with a credit limit of $1000 or more is " . $totalCustomersNevadaNewYorkNotUSWithHighCreditLimit[0]["total"] . ".</p>";
+echo "<br>";
+
+
+// Question 12
+echo "<h3>Question 12: How many customers don't have an assigned sales representative?</h3>";
+
+$sql = "SELECT COUNT(*) as total FROM customers WHERE salesRepEmployeeNumber IS NULL";
+$result = mysqli_query($conn, $sql);
+$totalCustomersWithoutSalesRepresentative = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>The amount of customers without an assigned sales representative is " . $totalCustomersWithoutSalesRepresentative[0]["total"] . ".</p>";
+echo "<br>";
+
+
+// Question 13
+echo "<h3>Question 13: How many orders have a comment?</h3>";
+
+$sql = "SELECT COUNT(*) as total FROM orders WHERE comments IS NOT NULL";
+$result = mysqli_query($conn, $sql);
+$ordersWithComment = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>The amount of orders with a comment is " . $ordersWithComment[0]["total"] . ".</p>";
+echo "<br>";
+
+
+// Question 14
+echo "<h3>Question 14: How many orders do we have where the comment mentions the word 'caution'?</h3>";
+
+$sql = "SELECT COUNT(*) as total FROM orders WHERE comments LIKE '%caution%'";
+$result = mysqli_query($conn, $sql);
+$ordersWithCommentContainingCaution = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>The amount of orders with a comment that contains the word 'caution' is " . $ordersWithCommentContainingCaution[0]["total"] . ".</p>";
+echo "<br>";
+
+
+// Question 15
+echo "<h3>Question 15: What is the average credit limit of our customers from the USA? (rounded)</h3>";
+
+$sql = "SELECT ROUND(AVG(creditLimit)) FROM customers WHERE COUNTRY='USA'";
+$result = mysqli_query($conn, $sql);
+$averageUSCustomerCreditLimit = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<p>The average credit limit of the custommers is " . $averageUSCustomerCreditLimit[0]["ROUND(AVG(creditLimit))"] . ".</p>";
+echo "<br>";
+
+
+// Question 16
+echo "<h3>Question 16: What is the most common last name from our customers?</h3>";
+
+$sql = "SELECT contactLastName, COUNT(*) as total FROM customers GROUP BY contactLastName ORDER BY COUNT(*) DESC LIMIT 1";
+$result = mysqli_query($conn, $sql);
+$mostCommonLastName= mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<p>The most common last name of the custommers is " . $mostCommonLastName[0]["contactLastName"] . ".</p>";
+echo "<br>";
+
+
+// Question 17
+echo "<h3>Question 17: What are valid statuses of the orders?</h3>";
+
+$sql = "SELECT DISTINCT status FROM orders";
+$result = mysqli_query($conn, $sql);
+$validOrderStatuses = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$validOrderStatusesArray = [];
+foreach ($validOrderStatuses as $validOrderStatus) {
+    array_push($validOrderStatusesArray, $validOrderStatus["status"]);
+}
+
+echo "<p>Valid order statuses are: " . implode(", ", $validOrderStatusesArray) . ".</p>";
+echo "<br>";
+
+
+// Question 18
+echo "<h3>Question 18: In which countries don't we have any customers?</h3>";
+
+$sql = "SELECT DISTINCT country FROM customers";
+$result = mysqli_query($conn, $sql);
+$allCountriesWithCustomers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$allCountriesWithCustomersArray = [];
+foreach ($allCountriesWithCustomers as $country) {
+    array_push($allCountriesWithCustomersArray, $country["country"]);
+};
+$searchedCountries = ["Austria", "Canada", "China", "Germany", "Greece", "Japan", "Philippines", "South Korea"];
+$noCustomersInTheseCountries = [];
+
+foreach($searchedCountries as $searchedCountry) {
+    if(!in_array($searchedCountry, $allCountriesWithCustomersArray)) {
+        array_push($noCustomersInTheseCountries, $searchedCountry);
+    }
+};
+
+echo "<p>The company has customers in: " . implode(", ", $noCustomersInTheseCountries) . ".</p>";
+echo "<br>";
+
+
+// Question 19
+echo "<h3>Question 19: How many orders where never shipped?</h3>";
+
+$sql = "SELECT COUNT(*) AS total FROM orders WHERE shippedDate IS NULL";
+$result = mysqli_query($conn, $sql);
+$amountOfNotShippedOrders = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<p>" . $amountOfNotShippedOrders[0]['total'] . " orders were never shipped.</p>";
+echo "<br>";
+
+
+// Question 20
+echo "<h3>Question 20: How many customers does Steve Patterson have with a credit limit above 100 000 EUR?</h3>";
+
+$sql = "SELECT COUNT(*) AS total FROM customers WHERE creditLimit > 100000 AND salesRepEmployeeNumber = (SELECT employeeNumber FROM employees WHERE firstName = 'Steve' AND lastName = 'Patterson')";
+$result = mysqli_query($conn, $sql);
+$stevePattersonCustomers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<p>Steve Patterson has " . $stevePattersonCustomers[0]['total'] . " customers wth a credit limit bigger than 100 000 EUR.</p>";
+echo "<br>";
+
+
+// Question 21
+echo "<h3>Question 21: How many orders have been shipped to our customers?</h3>";
+
+$sql = "SELECT COUNT(*) AS total FROM orders WHERE customerNumber 
+IN (SELECT customerNumber FROM customers) AND status = 'Shipped'";
+
+$result = mysqli_query($conn, $sql);
+$amountOfOrdersToOurCustomers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<p>" . $amountOfOrdersToOurCustomers[0]['total'] . " orders have been sent to our customers.</p>";
+echo "<br>";
+
+
+// Question 22
+echo "<h3>Question 22: On average, how many products do we have in each product line?</h3>";
+
+$sql = "SELECT ROUND((SELECT COUNT(*) FROM products) / (SELECT COUNT(*) FROM productlines)) AS average";
+$result = mysqli_query($conn, $sql);
+$averageProductLine = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>On average each product line has " . $averageProductLine[0]['average'] . " products.</p>";
+echo "<br>";
+
+// Question 23
+echo "<h3>Question 23: How many products are low in stock? (below 100 pieces)</h3>";
+
+$sql = "SELECT COUNT(*) AS total FROM products WHERE quantityInStock < 100";
+$result = mysqli_query($conn, $sql);
+$amountOfProductsLowInStock = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+echo "<p>" . $amountOfProductsLowInStock[0]['total'] . " products are low in stock.</p>";
+echo "<br>";
 ?>
 
